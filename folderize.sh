98d55d2f4b445b437059fa88ce246dfe2e5fe49e
dragonmarsx <64816372+dragonmarsx@users.noreply.github.com>
2024-05-21 13:25:42 -0500

    Removed hardcode folder name on folderize.sh

--- folderize.sh
+++ folderize.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
-mkdir LIBRARY
-if [ -d "LIBRARY" ];
-then
-  mkdir LIBRARY/AppData
-  mkdir LIBRARY/Documents
-  mkdir LIBRARY/Downloads
-  mkdir LIBRARY/Gallery
-  mkdir LIBRARY/Media
-  mkdir LIBRARY/Media/FamilyVideo
-  mkdir LIBRARY/Media/Movie
-  chmod -R 777 LIBRARY
-  ls -l LIBRARY
-else
-  echo -e "A problem exists creating "LIBRARY" folder.\n"; exit 0
+MY_DATAFOLDER="LIBRARY"
+
+if [[ $(whoami) -ne "root" ]]; then
+  echo -e "Please execute with root privileges!\n"; exit 0
 fi
-echo  "Folder structure created!"
+
+
+if [ -d "$MY_DATAFOLDER" ];
+then
+  echo -e "$MY_DATAFOLDER folder already exists!\n"; exit 0
+fi
+
+mkdir $MY_DATAFOLDER
+if [ -d "$MY_DATAFOLDER" ];
+then
+  mkdir $MY_DATAFOLDER/AppData
+  mkdir $MY_DATAFOLDER/Documents
+  mkdir $MY_DATAFOLDER/Downloads
+  mkdir $MY_DATAFOLDER/Gallery
+  mkdir $MY_DATAFOLDER/Media
+  mkdir $MY_DATAFOLDER/Media/FamilyVideo
+  mkdir $MY_DATAFOLDER/Media/Movie
+  chmod -R 777 $MY_DATAFOLDER
+  ls -l $MY_DATAFOLDER
+else
+  echo -e "A problem exists creating "$MY_DATAFOLDER" folder.\n"; exit 0
+fi
+echo  "DONE!"
